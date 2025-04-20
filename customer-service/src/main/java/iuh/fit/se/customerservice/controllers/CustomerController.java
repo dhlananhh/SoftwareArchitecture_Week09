0e6e8b979bf3da5c8e97600abadcd66644af98dc
Lan Anh <dhlananh2309@gmail.com>
2025-04-20 16:39:47 +0700

    refactor: update CustomerController

--- customer-service/src/main/java/iuh/fit/se/customerservice/controllers/CustomerController.java
+++ customer-service/src/main/java/iuh/fit/se/customerservice/controllers/CustomerController.java
@@ -1,42 +1,49 @@
 package iuh.fit.se.customerservice.controllers;
 
 import iuh.fit.se.customerservice.entities.Customer;
-import iuh.fit.se.customerservice.services.CustomerService;
+import iuh.fit.se.customerservice.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api/customers")
 public class CustomerController {
 
-    private CustomerService customerService;
-
     @Autowired
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+    private CustomerRepository customerRepository;
 
     @PostMapping
     public Customer createCustomer(@RequestBody Customer customer) {
-        return customerService.save(customer);
+        return customerRepository.save(customer);
     }
 
     @GetMapping("/{id}")
-    public Customer getCustomer(@PathVariable Long id) {
-        return customerService.findById(id);
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id) {
+        return customerRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping
-    public java.util.List<Customer> getAllCustomers() {
-        return customerService.getAllCustomers();
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customerDetails) {
+        return customerRepository.findById(id)
+                .map(customer -> {
+                    customer.setName(customerDetails.getName());
+                    customer.setAddress(customerDetails.getAddress());
+                    customer.setContact(customerDetails.getContact());
+                    return ResponseEntity.ok(customerRepository.save(customer));
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
-        customerService.deleteCustomer(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Object> deleteCustomer(@PathVariable Long id) {
+        return customerRepository.findById(id)
+                .map(customer -> {
+                    customerRepository.delete(customer);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
