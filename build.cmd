6a9f17e0bf4da69ce0caa3d4cb0c070669f8e61c
Lan Anh <dhlananh2309@gmail.com>
2025-04-20 15:27:47 +0700

    chore: update Dockerfiles for consistency and maintainability

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo Building the project with Maven in Docker...
 
-docker run --rm -v %cd%:/app -w /app maven:3.9.9-openjdk:latest mvn clean package -DskipTests
+docker run --rm -v %cd%:/app -w /app maven:3.8.5-openjdk:17-slim mvn clean package -DskipTests
 
 echo Build completed. Now you can run docker-compose up -d.
